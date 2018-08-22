f04ea0f212e847ba8fe93d32e1a341633539d7d8
Mikhail Toropov <loki@hp-mto.local>
2018-08-22 15:52:37 +0300

    Works version

--- upip.py
+++ upip.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
 print("Hello")
+import os
+moduls_list = []
+os.system("pip3 list > ~tmp.txt")
+with open("~tmp.txt", 'r') as file:
+    strings = file.readlines()
+for i in strings[2:]:
+    moduls_list.append(i.strip().split(' ')[0])
+
+for i in moduls_list:
+    os.system("pip3 install --upgrade "+i)
+    
+print("Update was successfully done")

07669df1a93e70104b787eb5257a61477ccbd062
Mikhail Toropov <mihail.toropov@gmail.com>
2018-08-22 21:55:56 +0300

    Optimize using memory

--- upip.py
+++ upip.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-print("Hello")
+print("Start upgrage all packeges installed by pip")
 import os
-moduls_list = []
 os.system("pip3 list > ~tmp.txt")
 with open("~tmp.txt", 'r') as file:
     strings = file.readlines()
 for i in strings[2:]:
-    moduls_list.append(i.strip().split(' ')[0])
+    os.system("pip3 install --upgrade "+i.strip().split(' ')[0])
 
-for i in moduls_list:
-    os.system("pip3 install --upgrade "+i)
-    
+os.system("echo 0 > ~tmp.txt")
 print("Update was successfully done")
